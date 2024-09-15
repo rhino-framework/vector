855e4e861437aa021420285109c79c1b1016d5a7
Artem Popov <popovar89@gmail.com>
2024-09-15 18:02:07 +0200

    Up version to 0.0.2

--- src/vector.hpp
+++ src/vector.hpp
@@ -14,7 +14,7 @@ class vector
 {
 public:
     using value_type = T;
-    using size_type = std::size_t;
+    using size_type = size_t;
     using pointer = value_type *;
     using const_pointer = value_type const *;
     using reference = value_type &;
